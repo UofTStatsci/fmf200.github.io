7ceee1d74bb8e4ddbb7e232f4b01f7459c7f9f4a
UofT Statsci <communications.statistics@utoronto.ca>
2026-09-07 18:03:41 -0400

    Create main.js

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+/* ==================================================
+   FMF200
+   The Future of Mathematical Finance
+   University of Toronto
+   ================================================== */
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("FMF200 website loaded");
+});
